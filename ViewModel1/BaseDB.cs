960b150407be30be44b8545a0d047f224a163aa6
Meamenn <yoav0108@gmail.com>
2025-04-02 15:08:25 +0300

    a lil prog on the viewmodel, still need to add all of the public statics to each class inherts from BaseDB, fix that stupid database prob, and fucking the game (WPF,WCF)!

--- ViewModel1/BaseDB.cs
+++ ViewModel1/BaseDB.cs
@@ -1,91 +1,102 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using Model;
 
 namespace ViewModel1.Data
 {
-    public abstract class BaseDB
+    public abstract class BaseDB<TEntity> where TEntity : BaseEntity, new()
     {
         protected readonly string ConnectionString;
-        protected SqlConnection Connection;
-        protected SqlCommand Command;
 
         protected BaseDB()
         {
             ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\Yoav\\source\\repos\\Meamenn\\Yoav-s-Pr\\Yoav's Pr\\App_Data\\Database1.mdf\";Integrated Security=True";
-            Connection = new SqlConnection(ConnectionString);
-            Command = new SqlCommand { Connection = Connection };
         }
 
-        protected abstract BaseEntity NewEntity();
-        protected abstract BaseEntity PopulateEntity(SqlDataReader reader);
+        protected abstract string GetTableName();
 
-        public List<BaseEntity> SelectAll()
+        public List<TEntity> SelectAll()
         {
-            var list = new List<BaseEntity>();
-            try
+            var list = new List<TEntity>();
+            using (var connection = new SqlConnection(ConnectionString))
+            using (var command = new SqlCommand($"SELECT * FROM {GetTableName()}", connection))
             {
-                Command.CommandText = $"SELECT * FROM {GetTableName()}";
-                Connection.Open();
-                using (var reader = Command.ExecuteReader())
+                try
                 {
-                    while (reader.Read())
+                    connection.Open();
+                    using (var reader = command.ExecuteReader())
                     {
-                        var entity = NewEntity();
-                        list.Add(PopulateEntity(reader));
+                        while (reader.Read())
+                        {
+                            var entity = new TEntity();
+                            list.Add((TEntity)PopulateEntity(reader));
+                        }
                     }
                 }
+                catch (Exception ex)
+                {
+                    throw new Exception($"Error selecting all records from {GetTableName()}: {ex.Message}", ex);
+                }
             }
-            catch (Exception ex) { Console.WriteLine($"Error: {ex.Message}"); }
-            finally { Connection.Close(); }
             return list;
         }
 
-        public BaseEntity SelectById(int id)
+        public TEntity SelectById(int id)
         {
-            try
+            using (var connection = new SqlConnection(ConnectionString))
+            using (var command = new SqlCommand($"SELECT * FROM {GetTableName()} WHERE Id = @Id", connection))
             {
-                Command.CommandText = $"SELECT * FROM {GetTableName()} WHERE Id = @Id";
-                Command.Parameters.Clear();
-                Command.Parameters.AddWithValue("@Id", id);
-                Connection.Open();
-                using (var reader = Command.ExecuteReader())
+                try
                 {
-                    if (reader.Read())
+                    command.Parameters.AddWithValue("@Id", id);
+                    connection.Open();
+                    using (var reader = command.ExecuteReader())
                     {
-                        var entity = NewEntity();
-                        return PopulateEntity(reader);
+                        if (reader.Read())
+                        {
+                            var entity = new TEntity();
+                            return (TEntity)PopulateEntity(reader);
+                        }
                     }
                 }
+                catch (Exception ex)
+                {
+                    throw new Exception($"Error selecting record with Id {id} from {GetTableName()}: {ex.Message}", ex);
+                }
             }
-            catch (Exception ex) { Console.WriteLine($"Error: {ex.Message}"); }
-            finally { Connection.Close(); }
             return null;
         }
 
         protected int ExecuteNonQuery(string sql, Dictionary<string, object> parameters = null)
         {
-            int records = 0;
-            try
+            using (var connection = new SqlConnection(ConnectionString))
+            using (var command = new SqlCommand(sql, connection))
             {
-                Command.CommandText = sql;
-                Command.Parameters.Clear();
-                if (parameters != null)
-                    foreach (var param in parameters)
-                        Command.Parameters.AddWithValue(param.Key, param.Value);
-                Connection.Open();
-                records = Command.ExecuteNonQuery();
+                try
+                {
+                    if (parameters != null)
+                    {
+                        foreach (var param in parameters)
+                        {
+                            command.Parameters.AddWithValue(param.Key, param.Value);
+                        }
+                    }
+                    connection.Open();
+                    return command.ExecuteNonQuery();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception($"Error executing query: {sql}. Error: {ex.Message}", ex);
+                }
             }
-            catch (Exception ex) { Console.WriteLine($"Error: {ex.Message}"); }
-            finally { Connection.Close(); }
-            return records;
         }
 
         public abstract void Insert(BaseEntity entity);
         public abstract void Update(BaseEntity entity);
         public abstract void Delete(BaseEntity entity);
 
-        protected abstract string GetTableName();
+        protected abstract BaseEntity PopulateEntity(SqlDataReader reader);
     }
 }
--- ViewModel1/SkinDB.cs
+++ ViewModel1/SkinDB.cs
@@ -1,155 +1,123 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Data.SqlClient;
 using Model;
 
-namespace ViewModel1.Data
+namespace Model
 {
-    public class SkinDB : BaseDB
+    public class Skin : BaseEntity
     {
-        private string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\Yoav\\source\\repos\\Meamenn\\Yoav-s-Pr\\Yoav's Pr\\App_Data\\Database1.mdf\";Integrated Security=True";
-        private SqlConnection connection;
-        private SqlCommand command;
-        private SqlDataReader reader;
+        public int Id { get; set; }
+        public string SkinName { get; set; }
+        public string Rarity { get; set; }
+        public int Cost { get; set; }
 
-        public SkinDB()
+        public Skin() { }
+
+        public Skin(int id, string skinName, string rarity, int cost)
         {
-            connection = new SqlConnection(connectionString);
-            command = new SqlCommand();
-            command.Connection = connection;
-        }
-
-        public List<Skin> SelectAll()
-        {
-            List<Skin> skinsList = new List<Skin>();
-
-            try
-            {
-                command.CommandText = "SELECT * FROM Skin";
-                connection.Open();
-                reader = command.ExecuteReader();
-
-                while (reader.Read())
-                {
-                    skinsList.Add(CreateModel());
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            finally
-            {
-                reader?.Close();
-                if (connection.State == ConnectionState.Open) connection.Close();
-            }
-
-            return skinsList;
-        }
-
-        public Skin SelectById(int id)
-        {
-            try
-            {
-                command.CommandText = $"SELECT * FROM Skin WHERE Id = {id}";
-                connection.Open();
-                reader = command.ExecuteReader();
-
-                if (reader.Read())
-                {
-                    return CreateModel();
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-            }
-            finally
-            {
-                reader?.Close();
-                if (connection.State == ConnectionState.Open) connection.Close();
-            }
-
-            return null;
-        }
-
-        //private override void CreateModel(BaseEntity entity)
-        //{
-        //    Skin skin = entity as Skin;
-        //    base.CreateModel(skin);
-        //}
-
-        public int Insert(Skin skin)
-        {
-            string sqlStr = $"INSERT INTO Skins (name, price) " +
-                            $"VALUES ('{skin.SkinName}', {skin.Cost})";
-            return SaveChanges(sqlStr);
-        }
-
-        public int Update(Skin skin)
-        {
-            string sqlStr = $"UPDATE Skins SET name='{skin.SkinName}', price={skin.Cost} " +
-                            $"WHERE Id={skin.Id}";
-            return SaveChanges(sqlStr);
-        }
-
-        public int Delete(Skin skin)
-        {
-            string sqlStr = $"DELETE FROM Skins WHERE Id={skin.Id}";
-            return SaveChanges(sqlStr);
-        }
-
-        protected override Model.BaseEntity newEntity()
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override Model.BaseEntity CreateModel(Model.BaseEntity entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override BaseEntity NewEntity()
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override BaseEntity PopulateEntity(SqlDataReader reader)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override void Insert(BaseEntity entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override void Update(BaseEntity entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public override void Delete(BaseEntity entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        protected override string GetTableName()
-        {
-            throw new NotImplementedException();
-        }
-
-        public class Skin
-        {
-            public int Id { get; set; }
-            public string SkinName { get; set; }
-            public string Rarity { get; set; }
-            public int Cost { get; set; }
+            Id = id;
+            SkinName = skinName;
+            Rarity = rarity;
+            Cost = cost;
         }
     }
 }
 
+namespace ViewModel1.Data
+{
+    public class SkinDB : BaseDB<Skin>
+    {
+        public SkinDB() : base()
+        {
+        }
+
+        public new List<Skin> SelectAll()
+        {
+            return base.SelectAll();
+        }
+
+        public new Skin SelectById(int id)
+        {
+            return base.SelectById(id);
+        }
+
+        public int Insert(Skin skin)
+        {
+            Insert(skin as BaseEntity);
+            return 1; // Assuming one row is inserted
+        }
+
+        public int Update(Skin skin)
+        {
+            Update(skin as BaseEntity);
+            return 1; // Assuming one row is updated
+        }
+
+        public int Delete(Skin skin)
+        {
+            Delete(skin as BaseEntity);
+            return 1; // Assuming one row is deleted
+        }
+
+        protected override BaseEntity PopulateEntity(SqlDataReader reader)
+        {
+            Skin skin = new Skin();
+            skin.Id = (int)reader["Id"];
+            skin.SkinName = reader["SkinName"].ToString();
+            skin.Rarity = reader["Rarity"].ToString();
+            skin.Cost = (int)reader["Cost"];
+            return skin;
+        }
+
+        public override void Insert(BaseEntity entity)
+        {
+            Skin skin = entity as Skin;
+            if (skin == null)
+                throw new ArgumentException("Entity must be of type Skin");
+
+            var parameters = new Dictionary<string, object>
+            {
+                { "@SkinName", skin.SkinName },
+                { "@Rarity", skin.Rarity },
+                { "@Cost", skin.Cost }
+            };
+            ExecuteNonQuery("INSERT INTO Skin (SkinName, Rarity, Cost) VALUES (@SkinName, @Rarity, @Cost)", parameters);
+        }
+
+        public override void Update(BaseEntity entity)
+        {
+            Skin skin = entity as Skin;
+            if (skin == null)
+                throw new ArgumentException("Entity must be of type Skin");
+
+            var parameters = new Dictionary<string, object>
+            {
+                { "@SkinName", skin.SkinName },
+                { "@Rarity", skin.Rarity },
+                { "@Cost", skin.Cost },
+                { "@Id", skin.Id }
+            };
+            ExecuteNonQuery("UPDATE Skin SET SkinName=@SkinName, Rarity=@Rarity, Cost=@Cost WHERE Id=@Id", parameters);
+        }
+
+        public override void Delete(BaseEntity entity)
+        {
+            Skin skin = entity as Skin;
+            if (skin == null)
+                throw new ArgumentException("Entity must be of type Skin");
+
+            var parameters = new Dictionary<string, object>
+            {
+                { "@Id", skin.Id }
+            };
+            ExecuteNonQuery("DELETE FROM Skin WHERE Id=@Id", parameters);
+        }
+
+        protected override string GetTableName()
+        {
+            return "Skin";
+        }
+    }
+}
